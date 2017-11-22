b9f088ebe44d0b6f83a8ee263103e3c83e01af81
Charles Peters <bkrubnzi@frankencloud.net>
2017-11-21 18:21:19 -0600

    Update and rename ad-hoc-ansible.sh to ansible-snippets.sh

--- ansible-snippets.sh
+++ ansible-snippets.sh
@@ -2,3 +2,5 @@
 ansible all -i "localhost," -m "debug" -a "var=hostvars[inventory_hostname]" -b
 #look for some files
 ansible all -i "localhost," -m "find" -a "path=/var/ossec/logs/ patterns='*.gz'" -b
+#concatenate a list into a string
+set_fact: ips=" {{instances.ip | join(',') }} "
