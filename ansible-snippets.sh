c0268e82ae90055d9becf8d1583236c71f4cc669
Charles Peters <bkrubnzi@frankencloud.net>
2017-11-21 18:37:40 -0600

    Update ansible-snippets.sh

--- ansible-snippets.sh
+++ ansible-snippets.sh
@@ -3,4 +3,13 @@ ansible all -i "localhost," -m "debug" -a "var=hostvars[inventory_hostname]" -b
 #look for some files
 ansible all -i "localhost," -m "find" -a "path=/var/ossec/logs/ patterns='*.gz'" -b
 #concatenate a list into a string
-set_fact: ips=" {{instances.ip | join(',') }} "
+  - set_fact: 
+      volume_list=" {{target_volumes.id | join(',') }}"
+#concatenate a set of strings into a list
+  - set_fact:
+      target_volumes: []
+  - set_fact:
+      target_volumes: "[ '{{ item.1.id }}' ] + {{ target_volumes }}"
+    with_subelements:
+      - "{{ log_volumes.results }}"
+      - volumes
