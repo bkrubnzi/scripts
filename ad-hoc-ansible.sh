2c678fbac1190aae55936f15e2ca4c3502d486f1
Charles Peters <bkrubnzi@frankencloud.net>
2017-11-21 18:19:09 -0600

    Update ad-hoc-ansible.sh

--- ad-hoc-ansible.sh
+++ ad-hoc-ansible.sh
@@ -1,2 +1,4 @@
 #Quick insight into the host
 ansible all -i "localhost," -m "debug" -a "var=hostvars[inventory_hostname]" -b
+#look for some files
+ansible all -i "localhost," -m "find" -a "path=/var/ossec/logs/ patterns='*.gz'" -b
