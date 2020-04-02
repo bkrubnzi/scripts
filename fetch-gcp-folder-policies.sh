5ebab0aa2153bc3bdeceecf9357f06a6eb0ac9d0
Charles Peters <bkrubnzi@frankencloud.net>
2020-04-02 12:28:37 -0500

    Create fetch-gcp-folder-policies.sh

---
+++ fetch-gcp-folder-policies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+find_children() {
+                while IFS= read -r sub_folder;do
+                        folder_name=`gcloud resource-manager folders describe $sub_folder --format json | jq -r '.displayName' | sed 's/\ /_/g'`
+                        mkdir $folder_name
+                        cd $folder_name
+                        folder_policy_filename=$sub_folder"_"$folder_name"_policy.json"
+                        gcloud resource-manager folders get-iam-policy $sub_folder --format json > $folder_policy_filename
+                        SUB_FOLDERS=`gcloud resource-manager folders list --folder $sub_folder --format json | jq -r '.[] | .name' | sed -e 's/.*\///'`
+                        if [ "$SUB_FOLDERS" ];then
+                                find_children $SUB_FOLDERS
+                        fi
+                        cd ..
+                done <<< "$SUB_FOLDERS"
+}
+
+ORG_ID=`gcloud organizations list --format json | jq -r '.[] | .name' | sed -e 's/.*\///'`
+ORG_NAME=`gcloud organizations describe $ORG_ID --format json | jq -r '.displayName'`
+mkdir $ORG_NAME
+cd $ORG_NAME
+SUB_FOLDERS=`gcloud resource-manager folders list --organization $ORG_ID --format json | jq -r '.[] | .name' | sed -e 's/.*\///'`
+find_children $SUB_FOLDERS
