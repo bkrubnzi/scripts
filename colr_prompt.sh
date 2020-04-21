e49014f28d4bbaccf07c66cc5b3bb3b764c464df
Charles Peters <bkrubnzi@frankencloud.net>
2020-04-21 12:23:04 -0500

    Create colr_prompt.sh

---
+++ colr_prompt.sh
@@ -0,0 +1,19 @@
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='\[\033[01;32m\][\u\[\033[01;33m\]@\h:\[\033[01;36m\]\W\[\033[01;32m\]]\$\[\033[00m\]'
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
