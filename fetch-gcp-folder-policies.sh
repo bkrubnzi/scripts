6a8c1e2e1dc6522cba35fb4aebe6bb157f1abdb6
Charles Peters <bkrubnzi@frankencloud.net>
2020-05-01 13:17:34 -0500

    Update fetch-gcp-folder-policies.sh

--- fetch-gcp-folder-policies.sh
+++ fetch-gcp-folder-policies.sh
@@ -7,6 +7,12 @@ find_children() {
                         cd $folder_name
                         folder_policy_filename=$sub_folder"_"$folder_name"_policy.json"
                         gcloud resource-manager folders get-iam-policy $sub_folder --format json > $folder_policy_filename
+                        PROJECT_IDS=`gcloud projects list --filter=" parent.id: '$sub_folder' " --format json | jq -r '.[] | .projectId'`
+                        if [ "$PROJECT_IDS" ]; then
+                                while IFS= read -r project_id;do
+                                        gcloud projects get-iam-policy $project_id --format json > $project_id.json
+                                done <<< "$PROJECT_NAMES"
+                        fi
                         SUB_FOLDERS=`gcloud resource-manager folders list --folder $sub_folder --format json | jq -r '.[] | .name' | sed -e 's/.*\///'`
                         if [ "$SUB_FOLDERS" ];then
                                 find_children $SUB_FOLDERS
